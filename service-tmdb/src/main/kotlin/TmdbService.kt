b0eff93f45a1e8f30199daaa85fa2c47e76be357
Arnold Szabo <szaboaarnold@gmail.com>
2019-11-23 16:56:37 +0200

    Improve TmdbService

--- service-tmdb/src/main/kotlin/TmdbService.kt
+++ service-tmdb/src/main/kotlin/TmdbService.kt
@@ -3,13 +3,20 @@ package com.cdev.service.tmdb
 import com.cdev.service.tmdb.model.TvSeries
 import com.cdev.service.tmdb.model.TvSeriesResult
 import io.ktor.client.HttpClient
+import io.ktor.client.call.HttpClientCall
 import io.ktor.client.call.call
 import io.ktor.client.call.receive
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.parameter
 import io.ktor.http.HttpMethod
 
 class TmdbService {
+
+    companion object{
+        val API_KEY = "df21461253277ed207247498065870cf"
+    }
 
     private val client = HttpClient() {
         install(JsonFeature) {
@@ -18,18 +25,21 @@ class TmdbService {
     }
 
     suspend fun getPopularContent(): ArrayList<TvSeries> {
-        val call =
-            client.call("https://api.themoviedb.org/3/tv/popular?api_key=df21461253277ed207247498065870cf&language=en-US&page=1") {
-                method = HttpMethod.Get
-            }
 
+        val call = client.get("/popular")
         val response = call.response.receive<TvSeriesResult>()
-
         val result = ArrayList<TvSeries>()
         for (tvSeries in response.result) {
             result.add(tvSeries)
         }
 
         return result
+    }
+
+    suspend fun HttpClient.get(url: String) : HttpClientCall{
+        return client.call("https://api.themoviedb.org/3/tv$url") {
+            method = HttpMethod.Get
+            parameter("api_key", API_KEY)
+        }
     }
 }
